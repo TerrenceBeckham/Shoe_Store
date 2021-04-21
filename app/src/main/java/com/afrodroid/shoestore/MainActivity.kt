b85a8abf71d9e0653dfe67af9ca737b78fac213b
terrencebeckham <terrencebeckham@icloud.com>
2021-04-21 08:49:07 +0200

    Added dataBinding and used dataBinding util to inflate main layout.

--- app/src/main/java/com/afrodroid/shoestore/MainActivity.kt
+++ app/src/main/java/com/afrodroid/shoestore/MainActivity.kt
@@ -2,12 +2,14 @@ package com.afrodroid.shoestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.afrodroid.shoestore.databinding.ActivityMainBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         Timber.plant(Timber.DebugTree())
         Timber.i("OnCreate Called")
     }
